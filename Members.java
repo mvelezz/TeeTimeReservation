d1f026e2629ac17fcf88e721a0598370ef3f3649
mvelezz <130081489+mvelezz@users.noreply.github.com>
2023-10-03 11:51:26 -0600

    Update Members.java
    Updated constructor

--- Members.java
+++ Members.java
@@ -1,18 +1,24 @@
 package teeTimeReservations;
 
-public class Members {
+public class Member {
 	int MemberNumber;
 	String fname;
 	String lname;
 	
 	String MemberStatus;
 	
-	//Members (int MemberNumber, String fname, String lname, String MemberStatus){
-		//this.MemberNumber = MemberNumber;
-		//this.fname  = fname;
-		//this.lname = lname;
-		//this.MemberStatus = MemberStatus;
-	//}
+	static Member member = null;
+	
+	Member(){
+		
+	}
+	
+	Member (int MemberNumber, String fname, String lname, String MemberStatus){
+		this.MemberNumber = MemberNumber;
+		this.fname  = fname;
+		this.lname = lname;
+		this.MemberStatus = MemberStatus;
+	}
 	
 	public void display() {
 		System.out.println("Member Number is: " + MemberNumber + " " + "and name is " + fname + " " + lname);
