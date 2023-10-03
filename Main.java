cd1b3f425e9208a3e47c79a7058cf767f04a7075
mvelezz <130081489+mvelezz@users.noreply.github.com>
2023-10-03 11:50:46 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ package teeTimeReservations;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Menu menuObject = new Menu();
-		menuObject.menu();
+		Menu.menu();
 		
 		
 
